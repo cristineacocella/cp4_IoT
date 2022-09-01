d881b585a8c1164794a50a13e2702ff6e53e34bd
Cristine Acocella <rm88251@fiap.com.br>
2022-09-01 20:37:41 -0300

    flows/arduino

---
+++ Arduino/DumpInfo_Cris.ino
@@ -0,0 +1,77 @@
+//CP4 2TDSJ
+//Cristine Acocella RM88251
+//Jonathan Felix RM 
+
+
+#include <SPI.h>
+#include <MFRC522.h>
+#include <Servo.h>
+
+/////Json
+#include <ArduinoJson.h>
+const int TAMANHO = 50;
+ 
+#define SS_PIN 10
+#define RST_PIN 9
+MFRC522 mfrc522(SS_PIN, RST_PIN);   
+Servo myServo;
+ 
+void setup() 
+{
+  Serial.begin(9600);  
+  Serial.setTimeout(10);
+  SPI.begin();      
+  mfrc522.PCD_Init();
+  myServo.attach(8);
+  myServo.write(0);
+  delay(1000);
+  myServo.write(180);
+ 
+}
+ 
+void loop() 
+{
+  StaticJsonDocument<TAMANHO> json; 
+  
+  if ( ! mfrc522.PICC_IsNewCardPresent()) 
+  {
+    return;
+  }
+  
+  if ( ! mfrc522.PICC_ReadCardSerial()) 
+  {
+    return;
+  }
+  
+
+  String conteudo= "";
+  byte letra;
+  for (byte i = 0; i < mfrc522.uid.size; i++) 
+  {
+     conteudo.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+     conteudo.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  conteudo.toUpperCase();
+    //formato de escrita do json
+  json["card"] = conteudo;
+  json["msg"] = " ";
+  json["aut"] = false;
+
+  serializeJson(json, Serial);
+  Serial.println();
+  
+  delay(1000);
+
+while (Serial.available() > 0) {
+    StaticJsonDocument<200> json2;
+    deserializeJson(json2, Serial);
+    if(json2.containsKey("aut")) {
+      myServo.write(0);
+      delay(1000);
+      myServo.write(180);
+    }
+  } 
+  
+  delay(1000);
+
+} 
---
+++ Arduino/temperatura.ino
@@ -0,0 +1,40 @@
+
+
+/////Json
+#include <ArduinoJson.h>
+const int TAMANHO = 50;  
+
+///// Sensor DTH
+#include "DHT.h"
+#define DHTPIN  7 
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE); 
+
+void setup() 
+{
+  //inicialia c sensor
+  dht.begin();
+
+  //inicializa comunicação serial
+  Serial.begin(9600);
+}
+
+void loop() 
+{
+  StaticJsonDocument<TAMANHO> json;
+
+  // Faz a leitura da temperatura  
+  float temp = dht.readTemperature();
+  // faz a leitura da humidade
+  float umi = dht.readHumidity();
+
+  //formato de escrita do json
+  json["temperatura"] = temp;
+  json["umidade"] = umi;
+
+  serializeJson(json, Serial);
+  Serial.println();
+
+  //delay
+  delay(9000);
+}
